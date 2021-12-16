f99c66e25e566d0be6000eec50730ce885fa4d45
Hrushi09 <74370312+Hrushi09@users.noreply.github.com>
2021-12-16 08:47:25 -0500

    Update signin.java

--- app/src/main/java/com/example/hotelapp/signin.java
+++ app/src/main/java/com/example/hotelapp/signin.java
@@ -1,4 +1,49 @@
-package com.example.hotelapp;
+package com.example.hra;
 
-public class signin {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class signin  extends AppCompatActivity{
+    private Button login;
+    private EditText Username;
+    private EditText Password;
+    private ImageButton Google;
+    private TextView ForgotPassword;
+    private TextView OrSigninwith;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+
+       login = (Button) findViewById(R.id.loginbtn);
+       Username = (EditText) findViewById(R.id.username);
+        Password= (EditText) findViewById(R.id.password);
+        Google = (ImageButton) findViewById(R.id.google);
+        ForgotPassword = (TextView) findViewById(R.id.forgotpassword);
+        OrSigninwith = (TextView) findViewById(R.id.signinwith);
+
+       login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( signin.this, hotelslist.class));
+                finish();
+            }
+        });
+       ForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( signin.this, ResetPassword.class));
+                finish();
+            }
+        });
+    }
 }
