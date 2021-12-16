22659f7ce532208381ebb3e5696a6f5396ae96c6
Paramjit99 <82598723+Paramjit99@users.noreply.github.com>
2021-12-15 21:13:42 -0500

    Update ResetPassword.java

--- app/src/main/java/com/example/hotelapp/ResetPassword.java
+++ app/src/main/java/com/example/hotelapp/ResetPassword.java
@@ -1,4 +1,40 @@
-package com.example.hotelapp;
+package com.example.hra;
 
-public class ResetPassword {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResetPassword extends AppCompatActivity {
+
+    private EditText Username;
+    private EditText CreatePassword;
+    private EditText ConfirmPassword;
+    private TextView ResetPassword;
+    private Button Reset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resetpassword);
+
+       Reset = (Button) findViewById(R.id.button6);
+        Username = (EditText) findViewById(R.id.username2);
+        CreatePassword= (EditText) findViewById(R.id.createpassword2);
+        ConfirmPassword = (EditText) findViewById(R.id.confirmpassword2);
+        ResetPassword = (TextView) findViewById(R.id.textView5);
+
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( ResetPassword.this, signin.class));
+                finish();
+            }
+        });
+
+    }
 }
