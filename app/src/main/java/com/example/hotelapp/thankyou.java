4a645dab7008c14b32e6d8ab71ec87c492d386e2
Paramjit99 <82598723+Paramjit99@users.noreply.github.com>
2021-12-16 00:06:32 -0500

    Update thankyou.java

--- app/src/main/java/com/example/hotelapp/thankyou.java
+++ app/src/main/java/com/example/hotelapp/thankyou.java
@@ -1,4 +1,33 @@
-package com.example.hotelapp;
+package com.example.hra;
 
-public class thankyou {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class thankyou extends AppCompatActivity {
+    private Button Quit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thankyou);
+        getSupportActionBar().setTitle("RentoGo");
+
+
+       Quit = (Button) findViewById(R.id.button4);
+
+
+        Quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( thankyou.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
 }
+
