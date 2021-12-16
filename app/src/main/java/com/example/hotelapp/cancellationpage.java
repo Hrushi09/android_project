459ab7355282e40bb5174c675b4b69502d58c7a1
sriya <reddysreya1@gmail.com>
2021-12-15 22:45:46 -0500

    Update cancellationpage.java

--- app/src/main/java/com/example/hotelapp/cancellationpage.java
+++ app/src/main/java/com/example/hotelapp/cancellationpage.java
@@ -1,4 +1,34 @@
-package com.example.hotelapp;
+package com.example.hra;
 
-public class cancellationpage {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class cancellationpage extends AppCompatActivity {
+    private Button Continue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cancellationpage);
+        getSupportActionBar().setTitle("RentoGo");
+
+
+        Continue = (Button) findViewById(R.id.button3);
+
+
+        Continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( com.example.hra.cancellationpage.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
 }
+
