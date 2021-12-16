130b2b69310349b73b545910b9f73982cd196863
sriya <reddysreya1@gmail.com>
2021-12-15 21:00:46 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/hotelapp/MainActivity.java
+++ app/src/main/java/com/example/hotelapp/MainActivity.java
@@ -1,14 +1,40 @@
-package com.example.hotelapp;
+package com.example.hra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signin;
+    private Button Createaccount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.homepage);
+        getSupportActionBar().setTitle("RentoGo");
+        signin =(Button) findViewById(R.id.signin);
+        Createaccount = (Button) findViewById(R.id.Createaccount);
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( MainActivity.this, signin.class));
+                finish();
+
+
+            }
+        });
+        Createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Createaccount.class));
+            }
+        });
     }
 }
