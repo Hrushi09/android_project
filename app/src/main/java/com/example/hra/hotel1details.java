9f78b240b07b45362c302872cd3ea1cb929ef971
Hrushi09 <74370312+Hrushi09@users.noreply.github.com>
2022-01-20 15:39:39 -0500

    hotel details

---
+++ app/src/main/java/com/example/hra/hotel1details.java
@@ -0,0 +1,42 @@
+package com.example.hra;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class hotel1details extends AppCompatActivity {
+    private Button Reserve;
+    private Button cancel;
+    private TextView Hotelplaza;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel1details);
+//        getSupportActionBar().setTitle("RentoGo");
+
+        Hotelplaza = (TextView) findViewById(R.id.hotelplaza);
+        Reserve = (Button) findViewById(R.id.reserve);
+       
+
+
+        Reserve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( hotel1details.this, thankyou.class));
+                finish();
+            }
+        });
+       cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( hotel1details.this, Cancellationpage.class));
+                finish();
+            }
+        });
+    }
+}
+
