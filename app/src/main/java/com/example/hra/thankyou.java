d7cfa0503ba1be19c0c60560babda5c5b282d538
Hrushi09 <74370312+Hrushi09@users.noreply.github.com>
2022-01-21 09:01:05 -0500

    Update thankyou.java

--- app/src/main/java/com/example/hra/thankyou.java
+++ app/src/main/java/com/example/hra/thankyou.java
@@ -1,32 +1,56 @@
 package com.example.hra;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class thankyou extends AppCompatActivity {
-    private Button Quit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thankyou);
-//        getSupportActionBar().setTitle("RentoGo");
+        getSupportActionBar().setTitle("RentoGo");
 
 
-       Quit = (Button) findViewById(R.id.button4);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Intent i1 = new Intent(getApplicationContext(), Bookings.class);
+                startActivity(i1);
+                break;
+            case R.id.item2:
+                Intent logout_intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(logout_intent);
+                break;
 
+            case R.id.item3:
+                ApplicationInfo api = getApplicationContext().getApplicationInfo();
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "CHECK out this cool application");
+                intent.putExtra(Intent.EXTRA_TEXT,"we are sharing information about our hotel plaza. for more details please visit our app RENTOGO and make your reservation ");
+                startActivity(Intent.createChooser(intent, "shareVia"));
+                break;
 
-        Quit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent( thankyou.this, MainActivity.class));
-                finish();
-            }
-        });
+        }
+        return true;
     }
 }
 
